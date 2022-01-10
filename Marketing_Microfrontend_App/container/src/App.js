1934903af16eabaee0a7c16f18138f0eccc7d86a
rupamswain1 <rupamswain1@gmail.com>
2022-01-10 16:04:00 +0530

    css issue is resolved and header is addded

--- Marketing_Microfrontend_App/container/src/App.js
+++ Marketing_Microfrontend_App/container/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { mount } from 'marketing/MarketingApp'
 import MarketingApp from './components/MarketingApp';
-console.log(mount);
+import Header from './components/Header';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core';
+
+const generateClassName=createGenerateClassName({
+    productionPrefix:'co'
+})
 export default ()=>{
     return (
         <div>
-            <h1>Hello From Container!!!</h1>
-            <MarketingApp/>
+            <StylesProvider generateClassName={generateClassName}>
+                <BrowserRouter>        
+                    
+                    <Header/>
+                    <MarketingApp/>
+                
+                </BrowserRouter>
+            </StylesProvider>
         </div>
+
         
         )
 };
